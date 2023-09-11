aba8ad3ed496868e49499b5df7917c523bb7774d
NicoLe-01 <nicosiahaan9898@gmail.com>
2023-09-11 15:32:38 +0800

    add: component to text fields and build a login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,37 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:testing_app/components/text_field.dart';
 
 class LoginPage extends StatelessWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Icon(
+                Icons.lock,
+                size: 100,
               ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
+              SizedBox(height: 50),
+              Text(
+                "welcome",
+                style: TextStyle(color: Colors.grey[600], fontSize: 16),
               ),
-            ),
-            SizedBox(height: 32.0),
-            ElevatedButton(
-              onPressed: () {
-                // Handle login logic here
-              },
-              child: Text('Login'),
-            ),
-          ],
+              SizedBox(
+                height: 25,
+              ),
+              MyTextField(
+                controller: usernameController,
+                hintText: "Username",
+                obscureText: false,
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              MyTextField(
+                controller: passwordController,
+                hintText: "Password",
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              
+            ],
+          ),
         ),
       ),
     );
